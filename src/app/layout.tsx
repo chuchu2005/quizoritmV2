0d3fe955dbe1f8b45e457d743e68cbabcf8b5392
Peter Okafor <69122271+chuchu2005@users.noreply.github.com>
2024-06-30 22:35:36 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 import Laoding from "@/components/ui/laoding";
 import { Providers } from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
-
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -19,7 +19,8 @@ export default function RootLayout({
         <body className={`${inter.className} bg-gray-50 text-gray-950 relative mt-5 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
           <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
           <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-            <Providers>
+            <GoogleAnalytics />
+          <Providers>
               <ClerkLoading>
                 <Navbar />
                 <Laoding />
