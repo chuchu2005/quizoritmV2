a50fd7645d0e1575f22ffd739a8818a7d151c9ae
iayvob <iayvob@gmail.com>
2024-08-01 16:56:14 +0100

    news

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { redirect } from "next/navigation";
 import QuizCreation from "@/components/QuizCreation";
 import { auth } from "@clerk/nextjs/server";
-import { useUser } from "@clerk/nextjs";
 import axios from "axios";
 
 export const metadata = {
@@ -18,9 +17,8 @@ interface Props {
 }
 
 const Quiz = async ({ searchParams }: Props) => {
-  const { user } = useUser();
   const { userId } = auth();
-  if (user && userId) {
+  if ( userId) {
     axios.post(`/api/getDetails`, { userId })
       .then((data) => {
         sessionStorage.setItem("plan", data.data.paymentData.plan);
